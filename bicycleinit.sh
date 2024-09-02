ba7c8cf80afc76eb308af8614ef9740ac14854ab
Lennart Ochel <lennart.ochel@outlook.com>
2024-09-02 13:49:41 +0200

    Add short delay to establish network connection

--- bicycleinit.sh
+++ bicycleinit.sh
@@ -8,6 +8,8 @@
 #
 # Update this script - bicycleinit.sh
 
+sleep 10
+
 # Navigate to the directory where the script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
